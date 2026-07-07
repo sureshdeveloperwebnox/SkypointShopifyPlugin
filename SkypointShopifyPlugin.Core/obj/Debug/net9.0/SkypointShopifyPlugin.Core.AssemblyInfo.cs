2e14ab53b9858141cf7876b7d92c53b7f7d99c53
sureshdeveloperwebnox <sureshdeveloperwebnox@gmail.com>
2026-07-07 11:58:46 +0530

    feat: add new UI layouts and assets to WebUI

--- SkypointShopifyPlugin.Core/obj/Debug/net9.0/SkypointShopifyPlugin.Core.AssemblyInfo.cs
+++ SkypointShopifyPlugin.Core/obj/Debug/net9.0/SkypointShopifyPlugin.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkypointShopifyPlugin.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05e8c6e8462a68aabfc509e0466d9aba658e6262")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e74a5e4842b17c8e75ae43209db86533d7de2e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkypointShopifyPlugin.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkypointShopifyPlugin.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
