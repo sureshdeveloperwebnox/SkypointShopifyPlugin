7bb23557ab7ba34cdd5ed45229110819955e8334
sureshdeveloperwebnox <sureshdeveloperwebnox@gmail.com>
2026-07-07 12:30:49 +0530

    feat: add auth.css and login layout components, and update build cache files

--- SkypointShopifyPlugin.Application/obj/Debug/net9.0/SkypointShopifyPlugin.Application.AssemblyInfo.cs
+++ SkypointShopifyPlugin.Application/obj/Debug/net9.0/SkypointShopifyPlugin.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkypointShopifyPlugin.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e14ab53b9858141cf7876b7d92c53b7f7d99c53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867a812ef30ed6ff2d2149ce0c2a0431df0b2713")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkypointShopifyPlugin.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkypointShopifyPlugin.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
